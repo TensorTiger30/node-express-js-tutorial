80d97a0d6db0d7c181b9b1e8609c3b5b8e47105f
Partho Ghosh <ghoshparth.96@gmail.com>
2024-09-14 18:09:05 +0530

    [Feat]: Explore fs read/write fileSync

---
+++ node/3-sync-fs-module.js
@@ -0,0 +1,13 @@
+const { readFileSync, writeFileSync } = require("fs");
+const path = require("path");
+
+const first = readFileSync(path.resolve("resources", "first.txt"), "utf8");
+const second = readFileSync(path.resolve("resources", "second.txt"), "utf8");
+
+console.log(first, second);
+
+writeFileSync(
+  path.resolve("resources", "sync.txt"),
+  `The resultant of the first and second file: ${first} and ${second} \n`,
+  { flag: "a" }
+);
